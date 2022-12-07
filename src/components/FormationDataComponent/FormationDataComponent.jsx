47c37eb660cfd2e0e284a143944687c3c03918b6
olivier-barbe <olivierbarbe59560@gmail.com>
2022-12-07 16:30:36 +0100

    ajout des formations détaillées

---
+++ src/components/FormationDataComponent/FormationDataComponent.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const FormationDataComponent = ({formationData}) => {
+    // console.log(formationData)
+
+    return (
+
+        <div>
+            <div>
+                <span>{`Description complète : ${formationData.descriptionDetail}`}</span>
+            </div>
+
+            <div>
+                <span>{`Lieux de la formation : ${formationData.lieux[0]} ou ${formationData.lieux[1]}`}</span>
+
+            </div>
+
+            <div>
+                <span>{`Nous proposons deux dates de début pour cette formation, le ${formationData.dates[0]} et ${formationData.dates[1]}`}</span>
+            </div>
+
+            <div>
+                Avant de commencer nos formations nous devons nous assurer  que vous avez les prérequis.
+                Voici un lien qui vous permettra de vous évaluer : <a href='https://www.w3schools.com/' target="_blank" rel="noreferrer">W3 School</a>
+            </div>
+
+        </div>
+
+    );
+}
+
+export default FormationDataComponent;
