701a7b7bd40efced68ce9b9bf5dc9f2f8f5a06d9
olivier-barbe <olivierbarbe59560@gmail.com>
2022-12-07 16:55:03 +0100

    ajout de css non fonctionnel

--- src/components/FormationDataComponent/FormationDataComponent.jsx
+++ src/components/FormationDataComponent/FormationDataComponent.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 
-const FormationDataComponent = ({formationData}) => {
+const FormationDataComponent = ({ formationData }) => {
     // console.log(formationData)
 
     return (
 
-        <div>
-            <div>
-                <span>{`Description complète : ${formationData.descriptionDetail}`}</span>
+        <div className='section'>
+            <div className='detail'>
+                <div>
+                    <span><b>{formationData.formationName}</b></span>
+                </div>
+
+                <div>
+                    <img className='img' src={formationData.logo} alt="Formation-logo" />
+                </div>
+
+                <div>
+                    <span>{`Description complète : ${formationData.descriptionDetail}`}</span>
+                </div>
+
+                <div>
+                    <span>{`Lieux de la formation : ${formationData.lieux[0]} ou ${formationData.lieux[1]}`}</span>
+
+                </div>
+
+                <div>
+                    <span>{`Nous proposons deux dates de début pour cette formation, le ${formationData.dates[0]} et ${formationData.dates[1]}`}</span>
+                </div>
+
+                <div>
+                    Avant de commencer nos formations nous devons nous assurer  que vous avez les prérequis.
+                    Voici un lien qui vous permettra de vous évaluer : <a href='https://www.w3schools.com/' target="_blank" rel="noreferrer">W3 School</a>
+                </div>
             </div>
 
-            <div>
-                <span>{`Lieux de la formation : ${formationData.lieux[0]} ou ${formationData.lieux[1]}`}</span>
-
-            </div>
-
-            <div>
-                <span>{`Nous proposons deux dates de début pour cette formation, le ${formationData.dates[0]} et ${formationData.dates[1]}`}</span>
-            </div>
-
-            <div>
-                Avant de commencer nos formations nous devons nous assurer  que vous avez les prérequis.
-                Voici un lien qui vous permettra de vous évaluer : <a href='https://www.w3schools.com/' target="_blank" rel="noreferrer">W3 School</a>
-            </div>
-
-        </div>
+        </div >
 
     );
 }
