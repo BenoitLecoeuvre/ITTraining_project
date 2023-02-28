699154d869101203ae6e36cc876ed55c8e8e11f4
Sebastien DUHEM <sduhem.pro@gmail.com>
2023-02-28 12:06:12 +0100

    fix branch sebastien2

--- webapi/Controllers/FormateurController.cs
+++ webapi/Controllers/FormateurController.cs
@@ -28,7 +28,6 @@ namespace webapi.Controllers
             return Ok(formateurs);
         }
 
-
         [HttpPost("/formateurs")]
         public async Task<IActionResult> AddFormateur([FromBody] Formateur formateur)
         {
@@ -40,23 +39,8 @@ namespace webapi.Controllers
             return BadRequest("Une erreur est survenue...");
         }
 
-        [HttpDelete("/formateurs/{id}")]
-        public async Task<IActionResult> Remove(int id)
-        {
-            var formateur = _formateurRepository.GetById(id);
-
-            if (formateur == null) return NotFound(new
-            {
-                Message = "Aucun formateur avec cet ID"
-            });
-
-            if (!await _formateurRepository.Delete(id)) return BadRequest("Une erreur est survenue...");
-
-            return Ok("Formateur supprimé");
-        }
-
         [HttpPut("/formateurs/{id}")]
-        public async Task<IActionResult> Update(int id, [FromBody] Formateur formateur)
+        public async Task<IActionResult> UpdateFormateur(int id, [FromBody] Formateur formateur)
         {
             var formateurFromDb = _dbContext.Formateurs.FirstOrDefault(p => p.Id == id);
 
@@ -72,6 +56,7 @@ namespace webapi.Controllers
                 formateurFromDb.Phone = formateur.Phone;
                 formateurFromDb.Status = formateur.Status;
                 formateurFromDb.Anciennete = formateur.Anciennete;
+                formateurFromDb.FormationsList = formateur.FormationsList;
                 if (_dbContext.SaveChanges() > 0)
                 {
                     return Ok("formateur modifié");
@@ -79,6 +64,21 @@ namespace webapi.Controllers
                 else return BadRequest("Une erreur est survenue...");
             }
 
+        }
+
+        [HttpDelete("/formateurs/{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var formateur = _formateurRepository.GetById(id);
+
+            if (formateur == null) return NotFound(new
+            {
+                Message = "Aucun formateur avec cet ID"
+            });
+
+            if (!await _formateurRepository.Delete(id)) return BadRequest("Une erreur est survenue...");
+
+            return Ok("Formateur supprimé");
         }
 
     }
